b4dc4a6ab01b07c5b3c757731b4973352250d24f
adlenoir <adlenoir@7c9264cf-d71d-4606-85b7-d7580034d5ec>
2017-04-06 21:04:12 +0000

    Ajout des fichiers ressources

---
+++ Appli Graphique/AppliGraphique/MyApp/Musique.cs
@@ -0,0 +1,67 @@
+﻿namespace MyApp
+{
+    internal class Musique
+    {
+        /*public string titre
+        {
+            get { return titre; }
+            private set { titre = value; }
+        }
+        public string artiste
+        {
+            get { return artiste; }
+            private set { artiste = value; }
+        }
+        public string infos
+        {
+            get { return infos; }
+            private set { infos = value; }
+        }
+        public string image
+        {
+            get { return image; }
+            private set { image = value; }
+        }
+        public string date
+        {
+            get { return date; }
+            private set { date = value; }
+        }
+        public string audio
+        {
+            get { return audio; }
+            private set { audio = value; }
+        }
+        public int duree
+        {
+            get { return duree; }
+            private set { duree = value; }
+        }*/
+
+        public string titre;
+
+        public string artiste;
+
+        public string infos;
+
+        public string image;
+
+        public string date;
+
+        public string audio;
+
+        public int duree;
+        
+
+        public Musique(string titre, string artiste, string infos, string image, string date, string audio, int duree)
+        {
+            this.titre = titre;
+            this.artiste = artiste;
+            this.infos = infos;          
+            this.image = image;
+            this.date = date;
+            this.audio = audio;
+            this.duree = duree;
+        }
+    }
+}
---
+++ Appli Graphique/AppliGraphique/MyApp/Window2.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyApp
+{
+    /// <summary>
+    /// Logique d'interaction pour Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Increase(object sender, RoutedEventArgs e)
+        {
+            Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            Canvas.SetTop(this, 0);
+            Canvas.SetLeft(this, 0);
+        }
+        
+        private void Explore(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.DefaultExt = "jpg";
+            openFile.Filter = "Image Files (*.png, *.jpg)|*.png;*.jpg";
+            openFile.ShowDialog();
+            if (openFile.FileNames.Length > 0)
+            {
+                explore.Content=openFile.FileNames[0];
+            }
+        }
+    }
+}
