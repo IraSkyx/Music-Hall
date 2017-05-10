f556c582e466bfb62de98190487a1afde547493a
adlenoir <adlenoir@7c9264cf-d71d-4606-85b7-d7580034d5ec>
2017-05-10 13:54:55 +0000

    - Retouches scalability - Correction de NullException

--- Appli Graphique/AppliGraphique/MyApp/MainWindow.xaml.cs
+++ Appli Graphique/AppliGraphique/MyApp/MainWindow.xaml.cs
@@ -314,7 +314,9 @@ namespace MyApp
 
         private void SeeMusic(object sender, MouseButtonEventArgs e)
         {
-            if(sender == listBox || sender == ActualPlay)
+            if (sender == listBox)
+                scroller.SelectedIndex = Allmusics.Index((Musique)listBox.SelectedItem);
+            if (sender == ActualPlay)
                 scroller.SelectedIndex = Allmusics.Index(Player.CurrentlyPlaying);
             else if (listBox.SelectedItem!=null && sender==scroller)
                 scroller.SelectedIndex = Allmusics.Index((Musique)listBox.SelectedItem);
--- Appli Graphique/AppliGraphique/MyApp/ScaleConverter.cs
+++ Appli Graphique/AppliGraphique/MyApp/ScaleConverter.cs
@@ -17,4 +17,14 @@ namespace MyApp
             throw new NotImplementedException();
         }
     }
+
+    public class ScaleConverter2 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (double)value * 0.1;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
